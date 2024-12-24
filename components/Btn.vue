fee7b5098b2026efcb06d562e1de3f3069e6fb6f
Elvin Kyungu <elvinkyungu.75@gmail.com>
2024-12-24 09:56:42 +0200

    feat: - Init project - create reusable customizable button component - And implement animated banner with GSAP

---
+++ components/Btn.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    :class="[
+      'p-2  rounded font-semibold border text-sm border-black transition-all duration-300',
+      bgColor,
+      hoverBgColor,
+      textColor,
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  bgColor: {
+    type: String,
+    default: 'bg-blue-500',
+  },
+  hoverBgColor: {
+    type: String,
+    default: 'hover:bg-blue-600', 
+  },
+  textColor: {
+    type: String,
+    default: 'text-white',
+  },
+});
+</script>
+
+<style scoped>
+</style>
